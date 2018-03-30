b7f3166a0697785bc6e844b9658a2d9fd1625020
JR Fent <jrfent@gmail.com>
2018-03-30 09:23:56 -0500

    Colton fixed and refactored

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,25 +1,25 @@
 class SubcategoriesController < ApplicationController
-  
+
+  before_filter :set_category
+  before_filter :set_subcategory, only: :show
+
   def show
-    @listings = Listing.where(subcategory_id: params[:id]).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
-    @category = Category.find(params[:category_id])
-    @subcategory = Subcategory.find(params[:id])
+    @listings = @subcategory.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
   end
-  
+
   def find_by_category
-    category = Category.find(params[:category_id])
-    subcategories = category.subcategories.find_all
-    render json: { subcategories: subcategories }
+    @subcategories = @category.subcategories.find_all
+    render json: { subcategories: @subcategories }
   end
-  
-  def state_category
-    category = Category.find(params[:category_id])
-  end
-  
+
   private
-  
-  def set_subcategory
-   @subcategory = Subcategory.find(params[:id])
-  end
-  
+
+    def set_category
+      @category = Category.find(params[:category_id])
+    end
+
+    def set_subcategory
+      @subcategory = Subcategory.find(params[:id])
+    end
+
 end
